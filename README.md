6eb73cd91218bd9d7e4c3331a0b76e73d620475b
Yewdev <Y_cooldev@outlook.com>
2023-03-17 01:24:41 +0700

    Update clean code for implement other projects

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 [👉 View Demo 👈](https://nextjs-simple-theme-switcher.vercel.app/)
 
 ### Using
+
 - [T3 Stack (Next.js + TailwindCSS)](https://create.t3.gg/)
 
 - [Next Themes](https://www.npmjs.com/package/next-themes) 📌
 
 - [Heroicons](https://github.com/tailwindlabs/heroicons)
-
+- [T3 Stack (Next.js + TailwindCSS)](https://create.t3.gg/)
--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -20,16 +20,12 @@ export default function ThemeToggle() {
         onClick={() =>
           currentTheme === "dark" ? setTheme("light") : setTheme("dark")
         }
-        className="flex items-center gap-2 rounded-3xl border-2 border-gray-600 py-2 px-4 text-black dark:border-gray-300 dark:text-white"
       >
         {currentTheme === "dark" ? (
-          <SunIcon className="w-12 text-yellow-200" />
+          <SunIcon className="w-20 text-yellow-200" />
         ) : (
-          <MoonIcon className="w-12 text-indigo-800" />
+          <MoonIcon className="w-20 text-indigo-500" />
         )}
-        <span className=" uppercase ">
-          {theme === "dark" ? "switch to light mode" : "switch to dark mode"}
-        </span>
       </button>
     </div>
   );
